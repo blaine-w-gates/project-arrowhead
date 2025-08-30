54e19e057da04848c4bfccdd2cb4b3e783938760
blaine-w-gates <129574424+blaine-w-gates@users.noreply.github.com>
2025-08-30 23:04:06 +0400

    chore(frontend): add ESLint config so CI can lint TypeScript

---
+++ website-integration/ArrowheadSolution/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-disable */
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    'playwright-report/',
+    'test-results/',
+    'drizzle/**',
+    'content/**',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  },
+};
